ab83b32c3ffe35b39e2da30bfded9913538f1f8b
ShengyiJia <sjia6@u.rochester.edu>
2020-07-20 02:31:07 -0400

    add code for insert digit anywhere
    not as clean as the python version due to the data structure

---
+++ java/insert_digit_anywhere/Main.java
@@ -0,0 +1,20 @@
+package insert_digit_anywhere;
+
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Util.init();
+
+        /* By changing the args[0] we can modify which prime we want to be the 
+         * first in the row. Same for the number of iteration.
+         */
+        Tree tree = new Tree(Integer.parseInt(args[0]), new ArrayList<Long>());
+        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+            tree.step();
+            Util.printList(tree.longestPath());
+        }
+    }
+}
